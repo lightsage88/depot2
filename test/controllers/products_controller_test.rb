bceb0ac82f6ce5e1bda79b3e43bb2a3bbe815d2b
lightsage88 <adrian.e.rosales@gmail.com>
2018-11-10 13:05:44 -0800

    on second time around, gained a better understanding of interplay between controller, views, their tests, and the use of yielding

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,4 +46,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "should have an h1 that says 'Products'" do
+    get products_url
+    assert_response :success
+    assert_select 'h1', "Products"
+  end
 end
