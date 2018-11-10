8ef8385e67c48ad0e6fa91d282f4ae3c97b5f446
lightsage88 <adrian.e.rosales@gmail.com>
2018-11-10 08:32:51 -0800

    got first tests working, second go around is feeling much clearer

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,26 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do 
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any? 
+    assert product.errors[:price].any? 
+    assert product.errors[:image_url].any?
+    end
+
+    test "price works as it should" do
+    product = Product.new(
+      title: "Some book",
+      description: "Pages with stuff",
+      image_url: "snore.jpg"              
+    )
+    product.price = -2 
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+    product.errors[:price]
+      product.price = 1
+      assert product.valid?
+    end
 end
