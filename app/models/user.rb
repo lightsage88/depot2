32bb8d5b2d6542cfaf4b73cb16be0c3b248de910
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-11-30 00:42:13 -0800

    completed chapter 15, going to try playgrounds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  
+  after_destroy :ensure_an_admin_exists
   has_secure_password
   #this line above creates a form taht prompts you
   #to ender a password and makes you reenter it in a
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   #typed what you thought you typed? That's exactly 
   #what has_secure_password() does. It tells Rails to
   #validate that the two passwords match.
+
+  class Error < StandardError
+  end
+
+  private
+    def ensure_an_admin_exists
+      if User.count.zero?
+        raise Error.new "Can't delete last user"
+      end
+    end
 end
